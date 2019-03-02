94b69a39aeadc7e5b537228d4ea3703821c7d898
upannala <upannala@gmail.com>
2019-03-02 12:42:44 +0530

    package json

---
+++ AppContainer.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+import React , {Component} from 'react';
+
+export default  class AppContainer extends  Component {
+
+    constructor(props){
+        super(props);
+
+    }
+     render(){
+        return <div>
+            <h2> hello sano </h2>
+    </div>;
+}
+
+}
